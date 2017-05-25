10f0b4b2e26a3480a151fe20327e89f5f598f682
EManicaro <enrico.manicaro.a103782@mcast.edu.mt>
2017-05-25 13:20:38 +0200

    Movement Working Good
    First Person Player movement and mouse follow. The player moved to the
    bedroom from where the game starts.

---
+++ Assets/Scripts/camMouseLook.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camMouseLook : MonoBehaviour
+{
+
+    // this will keep track of how much movement the camera done (will get the change of the movement since the last update)
+    Vector2 mouseLook;
+    // this will smooth the movement of the mouse
+    Vector2 smoothV;
+    // the sensitivity of the mouse in order to get any movement from the character
+    public float sensitivity = 5.0f;
+    // the amount of smoothness
+    public float smoothing = 2.0f;
+
+    // when the camera turns/move etc, it will control the whole body of the character
+    GameObject character;
+
+    void Start()
+    {
+        // character is set to the parent of the camera
+        character = this.transform.parent.gameObject;
+    }
+
+    void Update()
+    {
+        //md for mousedelta
+        var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        // the md is multiplied by the smoothing and the sensitivity of the mouse
+        md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+
+        //lerp - linear interpretation of movement, move smoothly between to points
+        smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f / smoothing);
+        smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f / smoothing);
+
+        //mouse look and smooth v are attached
+        mouseLook += smoothV;
+
+        // use the mouse look value and add the mouse look y as a local rotation to the camera around its y axis
+        // the minus in mouse look y, is an inverted system to rotate the camera up and down
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        //rotate according the he characters up and down not the camera, in order to move the whole character
+        character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
+    }
+}
---
+++ Assets/Scripts/characterController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class characterController : MonoBehaviour
+{
+    //public variable speed
+    public float speed = 10.0F;
+
+    // Use this for initialization
+    void Start()
+    {
+        // turn off cursor so we won't see it on screen and to stay in the game window
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    //Update is called once per frame
+    void Update()
+    {
+
+        //translation will be used for the up and down movements (get the value)
+        float translation = Input.GetAxis("Vertical") * speed;
+
+        //straffe is also like translate, but we will use it for the side movements (get the value)
+        float straffe = Input.GetAxis("Horizontal") * speed;
+
+        // to move smoothly between updates and at the same rate
+        translation *= Time.deltaTime;
+        straffe *= Time.deltaTime;
+
+        //move on the X and Y axis
+        transform.Translate(straffe, 0, translation);
+
+        // if escape key is pressed, lock state of the cursor will turn off
+        if (Input.GetKeyDown("escape"))
+            Cursor.lockState = CursorLockMode.None;
+    }
+}
