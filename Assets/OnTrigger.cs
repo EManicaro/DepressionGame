1c4038a6128541a5e6023be7167d7f7b56793009
ligrima <lianagrima@gmail.com>
2017-05-25 19:22:35 +0200

    kitchen and bathroom popups
    popups appear when the player enters the collider of the coffee machine,
    kitchen basin, mirror and bathroom basin and the toilet

--- Assets/OnTrigger.cs
+++ Assets/OnTrigger.cs
@@ -10,6 +10,17 @@ public class OnTrigger : MonoBehaviour {
     public GameObject LeaveBed;
     public GameObject PickRandomClothes;
     public GameObject CoordinateOutfit;
+    public GameObject Shave;
+    public GameObject NoShave;
+    public GameObject WashFaceAndTeeth;
+    public GameObject WashFace;
+    public GameObject ToiletSeatUp;
+    public GameObject ToiletSeatDown;
+    public GameObject ToastAndCoffee;
+    public GameObject Coffee;
+    public GameObject WashDishesNow;
+    public GameObject WashDishesLater;
+    
 
     public void Start()
     {
@@ -19,6 +30,16 @@ public class OnTrigger : MonoBehaviour {
         LeaveBed.SetActive(false);
         PickRandomClothes.SetActive(false);
         CoordinateOutfit.SetActive(false);
+        Shave.SetActive(false);
+        NoShave.SetActive(false);
+        WashFaceAndTeeth.SetActive(false);
+        WashFace.SetActive(false);
+        ToiletSeatUp.SetActive(false);
+        ToiletSeatDown.SetActive(false);
+        ToastAndCoffee.SetActive(false);
+        Coffee.SetActive(false);
+        WashDishesNow.SetActive(false);
+        WashDishesLater.SetActive(false);
     } 
 
     void OnTriggerEnter(Collider other)
@@ -40,6 +61,41 @@ public class OnTrigger : MonoBehaviour {
             PickRandomClothes.SetActive(true);
             CoordinateOutfit.SetActive(true);
         }
+
+        
+        //if (other.tag == "Mirror")
+       // {
+      //      Shave.SetActive(true);
+      //      NoShave.SetActive(true);
+      //  }
+
+        if (other.tag == "BathroomBasin")
+        {
+            Shave.SetActive(true);
+            NoShave.SetActive(true);
+            WashFaceAndTeeth.SetActive(true);
+            WashFace.SetActive(true);
+        }
+
+        if (other.tag == "Toilet")
+        {
+            ToiletSeatUp.SetActive(true);
+            ToiletSeatDown.SetActive(true);
+        }
+
+        if (other.tag == "CoffeeMachine")
+        {
+            ToastAndCoffee.SetActive(true);
+            Coffee.SetActive(true);
+        }
+
+        if (other.tag == "KitchenBasin")
+        {
+            WashDishesNow.SetActive(true);
+            WashDishesLater.SetActive(true);
+        }
+
+       
     }
 
     private void OnTriggerExit(Collider other)
@@ -60,6 +116,39 @@ public class OnTrigger : MonoBehaviour {
         {
             PickRandomClothes.SetActive(false);
             CoordinateOutfit.SetActive(false);
+        }
+
+
+        //if (other.tag == "Mirror")
+        //{
+        //Shave.SetActive(false);
+        //    NoShave.SetActive(false);
+        //}
+
+        if (other.tag == "BathroomBasin")
+        {
+            Shave.SetActive(false);
+            NoShave.SetActive(false);
+            WashFaceAndTeeth.SetActive(false);
+            WashFace.SetActive(false);
+        }
+
+        if (other.tag == "Toilet")
+        {
+            ToiletSeatUp.SetActive(false);
+            ToiletSeatDown.SetActive(false);
+        }
+
+        if (other.tag == "CoffeeMachine")
+        {
+            ToastAndCoffee.SetActive(false);
+            Coffee.SetActive(false);
+        }
+
+        if (other.tag == "KitchenBasin")
+        {
+            WashDishesNow.SetActive(false);
+            WashDishesLater.SetActive(false);
         }
     }
 
