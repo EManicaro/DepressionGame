6b0dc1d910557e2fc954ff96531ac2ffb25c6b49
ligrima <lianagrima@gmail.com>
2017-05-25 16:45:54 +0200

    pop ups - bedroom
    bedroom popups working fine, buttons does nt work at this stage due to
    the timer
    
    arranged colliders to make more sense

---
+++ Assets/OnTrigger.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTrigger : MonoBehaviour {
+    // target for reference
+    public GameObject Getup;
+    public GameObject Snooze;
+    public GameObject MakeBed;
+    public GameObject LeaveBed;
+    public GameObject PickRandomClothes;
+    public GameObject CoordinateOutfit;
+
+    public void Start()
+    {
+        Getup.SetActive(false);
+        Snooze.SetActive(false);
+        MakeBed.SetActive(false);
+        LeaveBed.SetActive(false);
+        PickRandomClothes.SetActive(false);
+        CoordinateOutfit.SetActive(false);
+    } 
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Alarm")
+        {
+            Snooze.SetActive(true);
+            Getup.SetActive(true);
+        }
+
+        if (other.tag == "Bed")
+        {
+            MakeBed.SetActive(true);
+            LeaveBed.SetActive(true);
+        }
+
+        if (other.tag == "Wardrobe")
+        {
+            PickRandomClothes.SetActive(true);
+            CoordinateOutfit.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Alarm")
+        {
+            Getup.SetActive(false);
+            Snooze.SetActive(false);
+        }
+
+        if (other.tag == "Bed")
+        {
+            MakeBed.SetActive(false);
+            LeaveBed.SetActive(false);
+        }
+
+        if (other.tag == "Wardrobe")
+        {
+            PickRandomClothes.SetActive(false);
+            CoordinateOutfit.SetActive(false);
+        }
+    }
+
+}
