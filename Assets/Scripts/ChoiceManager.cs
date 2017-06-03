8711ee6a2f500444a96b75111760e6478fa3ca53
Redd <redd.caruana@gmail.com>
2017-06-03 13:12:15 +0200

    [+] Added ChoiceManager script, to be attached to Triggers with the tag "Choice".
    [+] Added management code to allow/disable player movement through
    triggers.
    
    [-] Removed redundant code from OnTrigger script.
    
    [!] Made minor adjustments to the scene, but will need to be polished
    further to ensure proper functionality.
    
    [?] Objects that should trigger events will need to be be given the
    "Choice" tag, and the Choice Manager Script.
    
    [?] Canvas elements will need to be parented to the Choice Manager
    Script.
    
    [?] Canvas Buttons should link to the triggering object, Choice
    Manager's Trigger() function instead of the last two options. This will
    ensure consistent functionality for the player.

---
+++ Assets/Scripts/ChoiceManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChoiceManager : MonoBehaviour {
+
+	// was the event triggered in this scene?
+	bool wasTriggered;
+
+	// the objects that need to be activated/deactivated
+	public GameObject[] links;
+
+
+	void Start()
+	{
+		SetObjectsActive(false);	
+	}
+
+	// toggles the object's active state
+	void SetObjectsActive(bool active)
+	{
+		foreach (GameObject go in links)
+		{
+			go.SetActive(active);
+		}
+	}
+
+
+	// triggers the objects once for this scene
+	public void Trigger()
+	{
+		if (!wasTriggered)
+		{
+			wasTriggered = true;
+			SetObjectsActive(true);
+
+			GameObject.FindObjectOfType<camMouseLook>().CanPlayerLook(false);
+			GameObject.FindObjectOfType<characterController>().CanPlayerMove(false);
+		}
+		else
+		{
+			SetObjectsActive(false);
+			GameObject.FindObjectOfType<camMouseLook>().CanPlayerLook(true);
+			GameObject.FindObjectOfType<characterController>().CanPlayerMove(true);
+		}
+	}
+
+}
