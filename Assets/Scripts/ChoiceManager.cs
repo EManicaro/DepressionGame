12555f9492697f01e1e455db08d8a35aa66915ea
ligrima <lianagrima@gmail.com>
2017-06-11 19:40:36 +0200

    gui updates
    -added 2 other character controllers for day 3 and 4
    -made the character wake up at 8 on day 3, at 9:15 on day 4
    - removed the break up letter from the table on day 3 and moved it to
    day 4
    - arranged desk and bed colliders
    - added outline to all fonts except the red ones
    - made sure all text is visible

--- Assets/Scripts/ChoiceManager.cs
+++ Assets/Scripts/ChoiceManager.cs
@@ -2,55 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChoiceManager : MonoBehaviour {
+public class ChoiceManager : MonoBehaviour
+{
 
-	// was the event triggered in this scene?
-	bool wasTriggered;
+    // was the event triggered in this scene?
+    bool wasTriggered;
 
-	// the objects that need to be activated/deactivated
-	public GameObject[] links;
-
-
-	void Start()
-	{
-		SetObjectsActive(false);	
-	}
-
-	// toggles the object's active state
-	void SetObjectsActive(bool active)
-	{
-		foreach (GameObject go in links)
-		{
-			go.SetActive(active);
-		}
-	}
+    // the objects that need to be activated/deactivated
+    public GameObject[] links;
 
 
-	// triggers the objects once for this scene
-	public void Trigger()
-	{
-		Trigger(true);
-	}
+    void Start()
+    {
+        SetObjectsActive(false);
+    }
 
-	public void Trigger(bool disablePlayerControl)
-	{
-		if (!wasTriggered)
-		{
-			wasTriggered = true;
-			SetObjectsActive(true);
+    // toggles the object's active state
+    void SetObjectsActive(bool active)
+    {
+        foreach (GameObject go in links)
+        {
+            go.SetActive(active);
+        }
+    }
 
-			if (disablePlayerControl)
-			{
-				GameObject.FindObjectOfType<camMouseLook>().CanPlayerLook(false);
-				GameObject.FindObjectOfType<characterController>().CanPlayerMove(false);
-			}
-		}
-		else
-		{
-			SetObjectsActive(false);
-			GameObject.FindObjectOfType<camMouseLook>().CanPlayerLook(true);
-			GameObject.FindObjectOfType<characterController>().CanPlayerMove(true);
-		}
-	}
 
+    // triggers the objects once for this scene
+    public void Trigger()
+    {
+        Trigger(true);
+    }
+
+    public void Trigger(bool disablePlayerControl)
+    {
+        if (!wasTriggered)
+        {
+            wasTriggered = true;
+            SetObjectsActive(true);
+
+            if (disablePlayerControl)
+            {
+                GameObject.FindObjectOfType<camMouseLook>().CanPlayerLook(false);
+                GameObject.FindObjectOfType<characterController>().CanPlayerMove(false);
+            }
+        }
+        else
+        {
+            SetObjectsActive(false);
+            GameObject.FindObjectOfType<camMouseLook>().CanPlayerLook(true);
+            GameObject.FindObjectOfType<characterController>().CanPlayerMove(true);
+        }
+
+    }
 }
---
+++ Assets/Scripts/characterController2.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class characterController2 : MonoBehaviour
+{
+    //public variable speed
+    public float speed = 10.0F;
+
+
+    //time variables
+
+    public int hours = 9;
+    public int minutes = 15;
+
+    //this is the canvas text box
+    public Text timeText;
+
+	bool canMove = true;
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        // turn off cursor so we won't see it on screen and to stay in the game window
+        Cursor.lockState = CursorLockMode.Locked;
+        StartCoroutine("AutoTime");
+
+    }
+
+    //Update is called once per frame
+    void Update()
+    {
+
+		if (!canMove) return;
+
+        //translation will be used for the up and down movements (get the value)
+        float translation = Input.GetAxis("Vertical") * speed;
+
+        //straffe is also like translate, but we will use it for the side movements (get the value)
+        float straffe = Input.GetAxis("Horizontal") * speed;
+
+        // to move smoothly between updates and at the same rate
+        translation *= Time.deltaTime;
+        straffe *= Time.deltaTime;
+
+        //move on the X and Y axis
+        transform.Translate(straffe, 0, translation);
+
+
+        //new timer
+    }
+
+    //function for the time where the addition takes place
+    public void MoveTimeFwd(int m)
+    {
+
+        minutes += m;
+        int dMinutes = minutes % 60;
+
+
+        hours = (hours + ((minutes - dMinutes) / 60)) % 24;
+        minutes = dMinutes;
+
+		timeText.text = string.Format("Time: {0}:{1}", hours.ToString("0#"), minutes.ToString("0#"));
+    }
+
+
+    IEnumerator AutoTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(5);
+            MoveTimeFwd(1);
+        }
+    }
+
+	public void CanPlayerMove(bool move)
+	{
+		canMove = move;
+	}
+}
