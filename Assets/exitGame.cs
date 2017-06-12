171ba6ae119af0365ccd36f14268fa4b8cbc20f6
ligrima <lianagrima@gmail.com>
2017-06-12 04:42:36 +0200

    roofs + guis
    arranged roofs, last scene and some guis

---
+++ Assets/exitGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class exitGame : MonoBehaviour {
+
+    public void quit()
+    {
+        if (Input.GetKey("escape"))
+            Application.Quit();
+
+    }
+}
